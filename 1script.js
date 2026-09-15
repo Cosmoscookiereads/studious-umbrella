8a4322314d25da747bc007ebbcfc43fd9349b6b9
Cosmoscookiereads <purpleroseyuj@gmail.com>
2026-09-16 01:28:48 +0800

    I FOUND IT MUAHAHHAHAH IT WAS THE HTML AGAIN.
    Hopefully the divs will be moveable if not... I don't know what to do. ( ASKING FOR HELPPPPPPP AUAGHHHH )

--- 1script.js
+++ 1script.js
@@ -1,5 +1,38 @@
 
-dragElement(document.getElementById("welcomeaboardthedreamyexpress!"));
+dragElement(document.getElementById("welcomeaboardthedreamyexpress"));
+function dragElement(element) {
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+   if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+        initialX = e.clientX;
+    initialY = e.clientY;
+        document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+    function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+        currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+    function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("DreamJellyCookieWelcomesyou"));
 function dragElement(element) {
   var initialX = 0;
   var initialY = 0;
