1f49812451dfd870dcfa3916c7f24f4696fcb018
Cosmoscookiereads <purpleroseyuj@gmail.com>
2026-09-21 19:33:24 +0800

    IS IT YOU AGAIN >>:C
    I CANT SEE WHY STUFF WTH 
    ( the draggables and moveables and stuff of teh coolest ) 
    - Added teh intropost's draggable div functions ( its a test )

--- 1script.js
+++ 1script.js
@@ -1,18 +1,36 @@
+//Milkyway Cookie and Dreamjelly welcome aboard the dreamy express train intro! :
+dragElement(document.getElementById("welcomeaboardthedreamyexpress"));
 
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
 
-<div>
-       <p> Clock </p>
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-       <script>
-       <p> Conductor's pocket stopwatch </p>
-       
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
 
-          var Next,Departure = new Date().toLocalString();
-     function updateTime() { variable } { variable currentTime = new Date().toLocaleString();var timeText = document.querySelector("#timeElement");timeText.innerHTML = currentTime; setInterval(updateTime, 1000);}
-
-</script>
-  
-
-
-        
-     </div>}
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
